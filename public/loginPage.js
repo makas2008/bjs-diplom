58eb6f6ab2b74d6619539258e1221ed48511c945
Николай <rakach2009@mail.ru>
2025-01-16 21:58:21 +0300

    Update loginPage.js

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,19 +1,13 @@
 "use strict";
 
 const userForm = new UserForm();
-
-// ---------- 1. Авторизация на сайте (sign in): ---------- 
+ 
 userForm.loginFormCallback = data => {
-  // { login: "oleg@demo.ru", password: "demo" } <- data
   ApiConnector.login(data, response => {
-    // Варианты response:
-    // {success: true, userId: 1}
-    // {success: false, error: "Пользователь с логином и указанным паролем не найден"}
     response.success ? location.reload() : userForm.setLoginErrorMessage(response.error);
   });
 };
 
-// ----------  2. Регистрация на сайте (sign up): ---------- 
 userForm.registerFormCallback = data => {
   ApiConnector.register(data, response => {
     response.success ? location.reload() : userForm.setRegisterErrorMessage(response.error);
